ea4090c07805fe91dd26d0ea1010144bcd0f753c
Megha Purohit <mascotmegha.2012@gmail.com>
2021-04-19 22:18:57 -0400

    updated four files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import { Button } from '@material-ui/core';
 import Navbar from "./Components/Navbar";
-import MyCard from './Components/MyCard';
+
+import MyCard from "./Components/MyCard";
 import {getMatches} from './API/Api';
+import {
+  Grid
+} from "@material-ui/core";
 function App() {
+  const [matches,setMatches] = useState([]);
+  
 
   useEffect(() => {
     getMatches()
-      .then((data) => console.log("DATA",data))
+      .then((data) => setMatches(data.matches))
       .catch();
     }, []);
 
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <MyCard></MyCard>
+      <Navbar/>
       <h1>Welcome to my Live Score App</h1>
-      
+      <Grid container>
+        <Grid sm="2"></Grid>
+        <Grid sm="8">
+        {
+        matches.map((match)=>(
+          <MyCard match="match"/>
+        ))
+      }
+
+        </Grid>
+
+      </Grid>
     </div>
   );
 }
--- src/Components/MyCard.js
+++ src/Components/MyCard.js
@@ -1,7 +1,52 @@
-import React from "react";
-
-const MyCard =()=>{
-    return "MyCard"
-
-}
-export default MyCard;
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Grid,
+    Typography,
+  } from "@material-ui/core";
+  import React from "react";
+  
+  const MyCard =()=>{
+  
+      const getMyCard= ()=>{
+          return(
+              
+              <Card style={{ marginTop: 15}}>
+                   <CardContent>
+            <Grid container justify="center" alignItems="center" spacing={4}>
+              <Grid item>
+                <Typography variant="h5">First Team</Typography>
+              </Grid>
+              <Grid item>
+                  <img
+                  style={{ width: 85}}
+                  src={require("../img/mg.png")}
+                  alt=""
+                  />
+              </Grid>
+              <Grid item>
+                <Typography variant="h5">Second Team</Typography>
+              </Grid>
+            </Grid>
+          </CardContent>
+                  <CardActions>
+                      <Grid container justify="center" >
+                          <Button style={{ marginLeft: 10 }} item variant="contained" color="primary"> Show Details</Button>
+                      <Button style={{ marginLeft: 10 }} item variant="contained" color="primary">{new Date().toString()}</Button>
+                      </Grid>
+                      
+                  </CardActions>
+              </Card>
+          );
+      }
+          return getMyCard();
+  
+  }
+  export default MyCard;
